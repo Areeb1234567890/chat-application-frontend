64708ef001b245b5a7ae59f3befba8977620f699
Areeb <areebmhsn@gmail.com>
2024-06-21 20:35:12 +0500

    changing base url

--- src/context/socketProvider.js
+++ src/context/socketProvider.js
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 const SocketContext = createContext(null);
 
 export const SocketProvider = ({ children }) => {
-  const socket = useMemo(() => io("http://localhost:4000"), []);
+  const socket = useMemo(() => io("https://chat-application-backend-one.vercel.app"), []);
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
   );
